e4fcd4204ed91769ca7616e7dbb2b372f98e1505
Szilárd <salamonszilard@gmail.com>
2020-12-16 13:03:20 +0100

    névnap

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Client } from 'nevnapok';
+import { DatePipe } from '@angular/common';
+const nevnapok = new Client();
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [DatePipe]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  currentDate = new Date();
+  nameday : any;
+  constructor() { 
+    nevnapok.today()
+  .then(data => {
+    this.nameday = data;
+  })}
 
   ngOnInit() {
+    
   }
+  
 
 }
